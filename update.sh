9fe26d4c8cd8375f9fa047282e68beb36cec0709
coderHDY <986005715@qq.com>
2021-11-11 19:26:26 +0800

    不改了

--- update.sh
+++ update.sh
@@ -12,6 +12,6 @@ do
     echo "完成更新:" `date +%H:%M:%S` >> log.txt
 
     # sleepTime=`expr 60 \* 60 \* 24`
-    sleepTime=`expr 60 \* 1`
+    sleepTime=`expr 60 \* 60 \* 24`
     sleep ${sleepTime}
 done
