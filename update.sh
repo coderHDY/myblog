60388a647fee9781b941a516823f806179bbc593
coderHDY <986005715@qq.com>
2021-11-19 17:15:47 +0800

    自动化更新方案变更

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
-while true
-do
+update() {
     git pull
-    # npm i
+    #npm i
     npm run build
 
     mv dist dist_old
@@ -10,7 +9,17 @@ do
     rm -rf ./dist_old
 
     echo "完成更新:" `date +"%Y-%m-%d %H:%M:%S"` >> log.txt
+}
 
-    sleepTime=`expr 60 \* 60 \* 24`
+update
+
+while true
+do
+    current=`date +%H`
+    if test ${current} -eq '03';then
+        update
+    fi
+
+    sleepTime=`expr 60 \* 58`
     sleep ${sleepTime}
 done
