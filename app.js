fa58536d44127a34f2e52e4d930d6535bb1646d8
coderHDY <986005715@qq.com>
2021-11-19 17:15:47 +0800

    自动化更新方案变更

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
-const path = require("path");
-const fs = require("fs");
-const http = require("http");
+const express = require('express');
+const app = express();
+app.use('/', express.static('dist'));
 const port = 8088;
-const server = http.createServer((request, response) => {
-    const reqPath = getRealPath(request.url);
 
-    const reqUrl = path.join(__dirname, "./dist", reqPath);
-
-    const responseFile = fs.readFileSync(reqUrl);
-    response.end(responseFile);
+app.listen(port, () => {
+  console.log(`search-motion-docs listening at ${port}`);
 });
-
-server.listen(port, (error) => {
-    if (error) {
-        fs.appendFile('log.txt', error, 'utf-8', () => {});
-    }
-});
-
-function getRealPath(reqUrl) {
-    return reqUrl.startsWith('/assets') || reqUrl.endsWith('.html') || reqUrl.endsWith('.js') ? reqUrl : path.join(reqUrl, "index.html");
-}
---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [
+    {
+      name   : "docs server",
+      script : "./app.js"
+    },
+    {
+      name   : "update shell",
+      script : "./update.sh"
+    }
+  ]
+}
