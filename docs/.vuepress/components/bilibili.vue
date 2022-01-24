bb7ed9fa644d9217e89fb930237a98a8a15895fe
coderHDY <huangdeyu2020@163.com>
2022-01-24 21:46:30 +0800

    目录调整

---
+++ docs/.vuepress/components/bilibili.vue
@@ -0,0 +1,30 @@
+<template>
+  <iframe :src='src' scrolling="no" border="0" frameborder="no" width="100%" framespacing="0" allowfullscreen="true"> </iframe>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      require: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+iframe {
+  height: 480px;
+}
+@media (max-width: 768px) {
+  iframe {
+    height: 300px
+  }
+}
+@media (max-width: 480px) {
+  iframe {
+    height: 250px
+  }
+}
+</style>
--- docs/front/algorithm/README.md
+++ docs/front/algorithm/README.md
@@ -1,6 +1,6 @@
 ---
 title: 栈
-date: 2021-12-28
+date: 2021-11-28
 categories:
  - 算法
 ---
