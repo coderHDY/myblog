79245f9d2890d69e063bfd9c6b70620badc5e2ee
coderHDY <huangdeyu2020@163.com>
2022-02-18 23:39:58 +0800

    动态规划

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -67,7 +67,6 @@ const front = {
             collapsable: false,
             children: [
                 'sort',
-                'string',
                 'greedy',
                 'dynamic',
                 'tree',
