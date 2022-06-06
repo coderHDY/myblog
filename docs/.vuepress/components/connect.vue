6566342df09282761b1b639523cd362965376f1d
coderHDY <huangdeyu2020@163.com>
2022-06-07 00:04:49 +0800

    connect组件

--- docs/.vuepress/components/connect.vue
+++ docs/.vuepress/components/connect.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container" @click="toggle">
-    <div v-for="(src, i) in assets" :key="i" class="item" :class="{show: showIdx === i}">
+    <div v-for="(src, i) in assets" :key="i" class="item" :class="{show: showIdx === i}"  :data-id="i">
         <img :src="src" :data-id="i">
     </div>
   </div>
@@ -21,7 +21,6 @@ export default {
   },
   methods: {
       toggle(e) {
-          console.log(this);
           this.showIdx = !e.target.hasAttribute('data-id') ? -1 
             : (+e.target.getAttribute('data-id') !== this.showIdx ? +e.target.getAttribute('data-id'): -1);
           e.stopImmediatePropagation();
@@ -48,14 +47,15 @@ export default {
     overflow: hidden;
 }
 .item {
-    flex: 1;
-    height: 90%;
-    transition: all .2s 0s ease-in;
-    margin-right: 10px;
-    text-align: center;
-    border-radius: 30px;
-    overflow: hidden;
+  flex: 1;
+  height: 90%;
+  transition: all .2s 0s ease-in;
+  margin-right: 10px;
+  text-align: center;
+  border-radius: 30px;
+  overflow: hidden;
 }
+
 .item:last-child {
     margin-left: 0;
 }
@@ -66,5 +66,29 @@ export default {
 .show {
     flex: 6;
     box-shadow: 0 0 20px #333;
+}
+@media screen and (max-width:700px) {
+  .container {
+    flex-direction: column;
+    width: 100%;
+    height: 80vh;
+    box-sizing: border-box;
+  }
+  .item {
+    width: 100%;
+    margin-right: 0;
+    margin-bottom: 5px;
+  }
+
+  .item:last-child {
+      margin-bottom: 0;
+  }
+  .item>img {
+      height: 100%;
+      width: auto;
+  }
+  .show {
+    flex: 8;
+}
 }
 </style>
