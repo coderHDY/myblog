40c4f618cd9ea00000f444bb1a19d221828122e3
coderHDY <huangdeyu2020@163.com>
2022-07-24 17:39:57 +0800

    加密功能测试

---
+++ docs/.vuepress/components/md5.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wrapper">
+    <input type="text" v-model="pwd">
+    <div>
+        <span class="secret-copy">{{ secret }}</span>
+    </div>
+  </div>
+</template>
+<script>
+import md5 from 'md5';
+
+export default {
+    data() {
+        return {
+            pwd: "",
+        }
+    },
+    computed:{
+        secret() {
+            return this.pwd.trim() === '' ? '' : md5(this.pwd);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.wrapper {
+    display: flow-root;
+    height: 80px;
+}
+.secret-copy {
+    user-select: all;
+}
+.secret-copy::selection {
+    color: rgb(78, 86, 248);
+}
+</style>
