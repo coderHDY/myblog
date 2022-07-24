54bb2e5435b6543605adc9cd2d42318cc1a06703
coderHDY <huangdeyu2020@163.com>
2022-07-24 21:59:49 +0800

    bubble bug修复

--- docs/.vuepress/plugins/bubble/src/Bubble.vue
+++ docs/.vuepress/plugins/bubble/src/Bubble.vue
@@ -17,22 +17,31 @@ export default {
         speed: [Number, null],
         timer: [Number, null],
     },
-    mounted() {
-        if (typeof window === "undefined") return;
-        if (!window._bubble) {
-            window._bubble = new BubbleEffect();
-        }
-        this.bubble = window._bubble;
-        if (this.x && this.y) {
-            this.bubble.fixBubble(this.x, this.y, this.speed, this.timer);
-        }
-        if (this.random) {
-            this.bubble.autoBubble(this.speed, this.timer);
+    methods: {
+        init() {
+            if (typeof window === "undefined") return;
+            if (!window._bubble) window._bubble = new BubbleEffect();
+            this.bubble = window._bubble;
+            if (this.x && this.y) this.bubble.fixBubble(this.x, this.y, this.speed, this.timer);
+            if (this.random) this.bubble.autoBubble(this.speed, this.timer);
+        },
+        clear() {
+            this.bubble?.stopAutoBubble();
+            this.bubble?.stopFixBubble();
+        },
+        visibilityListener() {
+            if (document.visibilityState === 'hidden') this.clear();
+            // 用户打开或回到页面
+            if (document.visibilityState === 'visible') this.init();
         }
     },
+    mounted() {
+        this.init();
+        document.addEventListener('visibilitychange', this.visibilityListener);
+    },
     beforeDestroy() {
-        this.bubble?.stopAutoBubble();
-        this.bubble?.stopFixBubble();
+        this.clear();
+        document.removeEventListener('visibilitychange', this.visibilityListener);
     }
 };
 </script>
